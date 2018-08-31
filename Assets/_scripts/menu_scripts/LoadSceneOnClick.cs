e01815e124ec495dd21a7592596f6ea8e4e24997
oldmerkum <marcum.219@osu.edu>
2018-08-31 12:25:03 -0400

    Finished Main Menu basics.
    Start button loads pong_base.
    Audio settings have no effect currently.

---
+++ Assets/_scripts/menu_scripts/LoadSceneOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadSceneOnClick : MonoBehaviour {
+    int startSceneIndex = 1;
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(delegate { LoadByIndex(startSceneIndex); });
+    }
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ Assets/_scripts/menu_scripts/QuitOnClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitOnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Button>().onClick.AddListener(Quit);
+	}
+	public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
