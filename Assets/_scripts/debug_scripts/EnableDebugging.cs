fb2acf7c8a9551be0e191efd77c58df84a5e29fe
oldmerkum <marcum.219@osu.edu>
2018-09-01 20:05:05 -0400

    added debug mode to game options in main menu

---
+++ Assets/_scripts/debug_scripts/EnableDebugging.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDebugging : MonoBehaviour {
+    public GameObject debugGUI;
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T)) debugGUI.SetActive(!debugGUI.activeSelf);
+	}
+}
---
+++ Assets/_scripts/debug_scripts/SetupDebugGUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupDebugGUI : MonoBehaviour {
+    private GameObject ball;
+    void Start()
+    {
+        ball = GameObject.FindGameObjectWithTag("ball");
+    }
+    void OnGUI()
+    {
+        //make background box
+        GUILayout.Box("Test Menu");
+        GUILayout.Box("ball position: " + ball.GetComponent<Rigidbody>().position);
+        GUILayout.Box("ball velocity: " + ball.GetComponent<Rigidbody>().velocity);
+        if (GUILayout.Button("Increase Ball Velocity"))
+        {
+            ball.GetComponent<Rigidbody>().velocity *= 1.2f;
+        }
+        if (GUILayout.Button("Decrease Ball Velocity"))
+        {
+            ball.GetComponent<Rigidbody>().velocity *= .8f;
+        }
+    }
+}
