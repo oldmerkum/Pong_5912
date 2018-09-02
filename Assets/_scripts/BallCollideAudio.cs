ec83495a2e1ed82faeacb75a322d9692d8d03d9f
bugjr1028 <bugjr1028@gmail.com>
2018-09-02 18:38:28 -0400

    pong_main and pong_ai audio and win state completed
    Added working audio and win state to pong_main and pong_ai

---
+++ Assets/_scripts/BallCollideAudio.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollideAudio : MonoBehaviour {
+	private string wall = "wall";
+	private string paddle = "paddle";
+	private string goal = "goal";
+	private GameObject audioManager;
+	private AudioSource paddleHit;
+	private AudioSource wallHit;
+	private AudioSource goalHit;
+
+	void Awake () {
+		audioManager = GameObject.Find ("AudioManager");
+		var audioSources = audioManager.GetComponents<AudioSource> ();
+		paddleHit = audioSources [0];
+		wallHit = audioSources [1];
+		goalHit = audioSources [2];
+	}
+	void OnCollisionEnter(Collision ballCol)
+	{
+		if (ballCol.gameObject.CompareTag(paddle)) audioPlay(paddle);
+		if (ballCol.gameObject.CompareTag(wall)) audioPlay(wall);
+		if (ballCol.gameObject.CompareTag(goal)) audioPlay (goal);
+	}
+	void audioPlay(string audio)
+	{
+		if (audio == paddle) {
+			paddleHit.Play ();
+		}
+		if (audio == wall) {
+			wallHit.Play ();
+		}
+		if (audio == goal) {
+			goalHit.Play ();
+		}
+	}
+}
---
+++ Assets/_scripts/BallWinState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallWinState : MonoBehaviour {
+	private string goal = "goal";
+	private Vector3 restartForce = new Vector3 (5, 0, 0);
+	void OnCollisionEnter(Collision ballCol)
+	{
+		if (ballCol.gameObject.CompareTag(goal)) playerScore ();
+	}
+	void playerScore()
+	{
+		transform.localPosition = new Vector3 (-254.5f, -95.12941f, 0.0f);
+		GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		Invoke("restartBall", 2.0f);
+	}
+	void restartBall() {
+		int randSign = Random.Range (0, 2) * 2 - 1;
+		GetComponent<Rigidbody> ().AddForce (randSign*restartForce);
+	}
+}
