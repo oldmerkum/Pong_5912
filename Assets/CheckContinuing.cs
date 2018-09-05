07f9726c186ddf4b52e026597e2e55f73cdb9aa0
Tom K <kiener.17@osu.edu>
2018-09-05 03:02:48 -0700

    Saving
    -Remove audio sliders rather than make them work.
    -Add the ability to save state and continue from the main menu.

---
+++ Assets/CheckContinuing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckContinuing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (PlayerPrefs.GetInt("continue") == 1){
+            gameObject.SetActive(false);
+            GameObject.FindGameObjectWithTag("game_controller").GetComponent<GameController>().Load();              
+        }
+	}
+}
